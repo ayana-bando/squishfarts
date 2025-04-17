965426b2f8d750bc292466d3c50898c2e15962f6
ayana-bando <bandoayana@gmail.com>
2025-04-17 08:55:24 -0400

    readd removed files

---
+++ app/build/generated/source/buildConfig/debug/com/bando/android/squishfarts/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.bando.android.squishfarts;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.bando.android.squishfarts";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
---
+++ app/build/kotlin/compileDebugKotlin/caches-jvm/lookups/lookups.tab.values.s
@@ -0,0 +1,1 @@
+ÍpórÍ~ùxösösÞtîsâyÌ|åtãrÅnÉnÑnÑnÃoÀpÿnûmømõnøsüyñtïqøsðqèqæpöo÷oËrÒmÞrÄrèrÄs×pÓnêrßpÍq
